a39a371d0cf30a277a274851d40ac69b8dda9322
Alice Brunel <alc.brunel@gmail.com>
2019-07-21 09:00:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,26 @@
 class Dog 
   
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize (name)
+    @name = name
+    
+    @@all << name
+  end
+  
+  def self.all
+    
+  end
+  
+  def self.clear_all
+  
+  end
+  
+  def self.print_all
+  
+  end
   
   
 end
